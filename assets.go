bf4042e96db4164a1a7c594cc726a3f7c8b74cbe
42bitpotato <skickligt01.flagig@icloud.com>
2025-12-16 16:10:25 +0100

    Updated getAssetsPath to set filename to base64 encoded string

--- assets.go
+++ assets.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
-
-	"github.com/google/uuid"
 )
 
 func (cfg apiConfig) ensureAssetsDir() error {
@@ -16,9 +16,17 @@ func (cfg apiConfig) ensureAssetsDir() error {
 	return nil
 }
 
-func getAssetPath(videoID uuid.UUID, mediaType string) string {
+//func getAssetPath(videoID uuid.UUID, mediaType string) string {
+//	ext := mediaTypeToExt(mediaType)
+//	return fmt.Sprintf("%s%s", videoID, ext)
+//}
+
+func getAssetPath(mediaType string) string {
+	byteSlice := make([]byte, 32)
+	rand.Read(byteSlice)
+	fileName := base64.RawURLEncoding.EncodeToString(byteSlice)
 	ext := mediaTypeToExt(mediaType)
-	return fmt.Sprintf("%s%s", videoID, ext)
+	return fmt.Sprintf("%s%s", fileName, ext)
 }
 
 func (cfg apiConfig) getAssetDiskPath(assetPath string) string {
